a1ea9628e34c9551d5460740569f0d599afb366e
patricoferris <patrickferris17@gmail.com>
2020-10-16 16:49:00 +0100

    remove oraclelinux

--- test/printer.ml
+++ test/printer.ml
@@ -3,7 +3,7 @@ let detect_stuff () =
   let arch = Osrelease.Arch.v () in
   let distro = Osrelease.Distro.v () |> Rresult.R.get_ok in
   let ver = Osrelease.Version.v () |> Rresult.R.get_ok in
-  Printf.printf "%s %s %s %s\n%!"
+  Printf.printf "Operating System: %s\nArchitecture: %s\nDistribution: %s\nVersion: %s\n %!"
     (Osrelease.OS.to_string os)
     (Osrelease.Arch.to_string arch)
     (Osrelease.Distro.to_string distro)
